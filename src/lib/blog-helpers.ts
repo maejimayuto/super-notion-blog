277a115d4c4e1c403202afa004d137536c7a2e32
maejimayuto <maejimayuto@gmail.com>
2021-11-19 17:36:14 +0900

    add: just copy from ijjk/notion-blog

---
+++ src/lib/blog-helpers.ts
@@ -0,0 +1,31 @@
+export const getBlogLink = (slug: string) => {
+  return `/blog/${slug}`
+}
+
+export const getDateStr = (date: string | number | Date) => {
+  return new Date(date).toLocaleString('en-US', {
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
+  })
+}
+
+export const isPublished = (post: any) => {
+  return post.Published === 'Yes'
+}
+
+export function normalizeSlug(slug: string): any {
+  if (typeof slug !== 'string')
+    return slug
+
+  let startingSlash = slug.startsWith('/')
+  let endingSlash = slug.endsWith('/')
+
+  if (startingSlash) {
+    slug = slug.substr(1)
+  }
+  if (endingSlash) {
+    slug = slug.substr(0, slug.length - 1)
+  }
+  return startingSlash || endingSlash ? normalizeSlug(slug) : slug
+}
