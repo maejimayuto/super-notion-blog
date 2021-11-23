5f7ea4f11cf5d5138e31f656b326180e449962c9
maejimayuto <maejimayuto@gmail.com>
2021-11-23 21:34:32 +0900

    change: favicon

--- src/components/custome-head.tsx
+++ src/components/custome-head.tsx
@@ -5,7 +5,13 @@ const CustomeHead = ({ titlePre = '' }) => {
     <Head>
       <title>前島 悠人{titlePre ? ` | ${titlePre}` : ''}</title>
       <meta name="description" content="とっても自由な Web エンジニアがプロダクト開発でどこまでいけるかの挑戦を見守るサイトです。" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
     </Head>
   )
 }
