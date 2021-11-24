b81fe1de230fb20b7c2e3481453a7236b00b1846
maejimayuto <maejimayuto@gmail.com>
2021-11-24 21:47:37 +0900

    remove: css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,14 @@ import type { NextPage } from 'next'
 
 import CustomHead from '../components/custom-head'
 import Header from '../components/header'
-import styles from '../styles/home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <CustomHead titlePre="Home" />
       <Header />
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           前島 悠人
         </h1>
         <p className="text-blue-600">前島 悠人</p>
