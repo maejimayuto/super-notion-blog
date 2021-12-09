5265a41031a7cc152be11ef31a2c13b773009491
maejimayuto <maejimayuto@gmail.com>
2021-12-09 15:41:15 +0900

    fix: change topic path padding

--- src/components/topic-path.tsx
+++ src/components/topic-path.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
 
 const TopicPath = ({ paths = [] }) => {
   return (
-    <div className="flex items-center py-1 px-4 h-11 text-sm bg-white">
+    <div className="flex items-center py-1 h-11 text-sm bg-white">
       <Link href="/" passHref>
-        <a className="flex items-center py-1 px-1 hover:bg-gray-200 rounded">
-          <div className="ml-1">
+        <a className="flex items-center py-1 pr-1 hover:bg-gray-200 rounded">
+          <div>
             前島 悠人
           </div>
         </a>
