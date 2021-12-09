4b13c01a33ccbd476d605dcd966e18608bddb11a
maejimayuto <maejimayuto@gmail.com>
2021-12-09 14:08:40 +0900

    add =: bread list

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -50,6 +50,23 @@ const Index = ({ posts = [], preview }) => {
         </div>
       )}
       <div>
+        <div className="flex items-center py-1 px-4 h-11 text-sm bg-white">
+        <Link href="/" passHref>
+          <a className="flex items-center py-1 px-1 hover:bg-gray-200 rounded">
+            <div className="ml-1">
+              前島 悠人
+            </div>
+          </a>
+        </Link>
+        <div className="px-1">/</div>
+        <Link href="/blog" passHref>
+          <a className="flex items-center py-1 px-1 hover:bg-gray-200 rounded">
+            <div className="ml-1">
+              ✍️ Blog
+            </div>
+          </a>
+        </Link>
+      </div>
         <h1>My Notion Blog</h1>
         {posts.length === 0 && (
           <p className={blogStyles.noPosts}>🚧メンテナンス中🚧</p>
