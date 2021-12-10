7cda1db4b58b860b6496c67561dc9f0b6c94f67c
maejimayuto <maejimayuto@gmail.com>
2021-12-10 21:13:31 +0900

    remove div

--- src/components/topic-path.tsx
+++ src/components/topic-path.tsx
@@ -4,18 +4,14 @@ const TopicPath = ({ paths = [] }) => {
   return (
     <div className="flex items-center py-1 h-11 text-sm bg-white">
       <Link href="/" passHref>
-        <a className="flex items-center py-1 pr-1 hover:bg-gray-200 rounded">
-          <div>
-            前島 悠人
-          </div>
+        <a className="flex items-center py-1 px-1 hover:bg-gray-200 rounded">
+          前島 悠人
         </a>
       </Link>
       <div className="px-1 text-fg-2">/</div>
       <Link href="/blog" passHref>
         <a className="flex items-center py-1 px-1 hover:bg-gray-200 rounded">
-          <div className="ml-1">
-            {paths[0]}
-          </div>
+          {paths[0]}
         </a>
       </Link>
       {paths.length == 2 && (
@@ -23,9 +19,7 @@ const TopicPath = ({ paths = [] }) => {
           <div className="px-1 text-fg-2">/</div>
           <Link href="/" passHref>
             <a className="flex items-center py-1 px-1 hover:bg-gray-200 rounded">
-              <div className="ml-1">
-                {paths[1]}
-              </div>
+              {paths[1]}
             </a>
           </Link>
         </>
