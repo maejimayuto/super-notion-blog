cb9e57666eb41f945e66125e80cbb12323d40c7b
maejimayuto <maejimayuto@gmail.com>
2021-12-10 21:18:45 +0900

    fix: design title section

--- src/components/topic-path.tsx
+++ src/components/topic-path.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 
-const TopicPath = ({ paths = [] }) => {
+const TopicPath = ({ paths = [], className = "" }) => {
   return (
-    <div className="flex items-center py-1 h-11 text-sm bg-white">
+    <div className={`flex items-center py-1 h-11 bg-white ${className}`}>
       <Link href="/" passHref>
         <a className="flex items-center py-1 px-1 hover:bg-gray-200 rounded">
           前島 悠人
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -167,14 +167,33 @@ const RenderPost = ({ post, redirect, preview }) => {
       )}
       {/* <div className="px-4 pb-8 my-0 mx-auto max-w-3xl"> */}
       <div className={blogStyles.post}>
-        <TopicPaths paths={["✍️ Blog", post.Page]} />
-        {post.PageIcon}
-        <h1>{post.Page || ''}</h1>
-        {post.Date && (
-          <div className="posted">Posted: {getDateStr(post.Date)}</div>
-        )}
-
-        <hr />
+        <section>
+          <TopicPaths
+            paths={["✍️ Blog", post.PagePath]}
+            className="mb-3"
+          />
+          <h1 className="mb-8 text-4xl font-bold">{post.Page || ''}</h1>
+          <div className="grid grid-cols-4 gap-4 pt-1 pb-4 mb-4 text-sm text-fg-3 border-b">
+            <div className="flex col-span-1">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6"
+                fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                  d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              </svg>
+              <span className="ml-1">Created</span>
+            </div>
+            <div className="col-span-3">{getDateStr(post.Date)}</div>
+            <div className="flex col-span-1">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6"
+                fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                  d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
+              </svg>
+              <span className="ml-1">Tag</span>
+              </div>
+            <div className="col-span-3">{post.Tag}</div>
+          </div>
+        </section>
 
         {(!post.content || post.content.length === 0) && (
           <p>This post has no content</p>
