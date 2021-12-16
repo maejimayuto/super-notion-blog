9c672da1ec8452d92da3eb8a8dbb40f59925b82a
maejimayuto <maejimayuto@gmail.com>
2021-12-16 16:54:03 +0900

    add: space

--- src/lib/notion/renderers.ts
+++ src/lib/notion/renderers.ts
@@ -66,7 +66,7 @@ export function textBlock(text = [], noPTag = false, mainKey) {
     return React.createElement(
       'div',
       // TODO: tailwind が適用できなかった
-      { style: {margin: '0.08rem 0', padding: '0.16rem 0.12rem'} },
+      { style: { margin: '0.08rem 0', padding: '0.16rem 0.12rem' } },
       React.createElement(
         components.p,
         { key: mainKey },
